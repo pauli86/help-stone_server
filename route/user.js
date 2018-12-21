d9486b5e900919896959b4ff21db8bd445fd9fac
paul_home_win10 <ghseok86@gmail.com>
2018-12-22 04:26:08 +0900

    mongoose error fix

--- route/user.js
+++ route/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express.Router();
 const crypto = require('crypto');
-
+const mongoose = require('mongoose');
 const User = require('../models/user');
 const Project = require('../models/project');
 
